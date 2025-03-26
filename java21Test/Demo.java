78fea47f6084fd87956a488ea28fb1c05a08f65a
zhangye <xianrenzy1024@163.com>
2025-03-26 14:17:45 +0800

    1. 冒泡排序、选择排序 2. java21的代码块

---
+++ java21Test/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+        String str = """
+                1234567890
+                1234567890
+                1234567890
+        """;
+        System.out.println(str);
+	}
+
+}
